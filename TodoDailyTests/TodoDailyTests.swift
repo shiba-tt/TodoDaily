8a90566eafb2b4da166d235235c91acd7075696b
shiba-tt <141482658+shiba-tt@users.noreply.github.com>
2025-05-26 00:32:25 +0900

    Initial commit

---
+++ TodoDailyTests/TodoDailyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDailyTests.swift
+//  TodoDailyTests
+//
+//  Created by Tatsuya Shiba on 2025/05/26.
+//
+
+import Testing
+@testable import TodoDaily
+
+struct TodoDailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
