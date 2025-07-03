da04690c0d144d6416bb7e82e0d7df7708aa8108
shiba-tt <141482658+shiba-tt@users.noreply.github.com>
2025-07-04 02:03:32 +0900

    設定Drawerのアニメーションとかは完成した。

---
+++ TodoDaily/Features/Settings/Views/SettingsView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+import SwiftData
+
+struct SettingsView: View {
+    // 表示/非表示を保持します。
+    @Binding
+    var isOpen: Bool
+    
+    // iPhoneの幅を保持します。
+    private let maxWidth: CGFloat = UIScreen.main.bounds.width
+    
+    var body: some View {
+        ZStack {
+            // isOpenで背景が透明な黒になるようにする
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+                .opacity(isOpen ? 0.7 : 0)
+                .onTapGesture {
+                    // isOpenの変化にアニメーションをつけます
+                    withAnimation(.easeIn(duration: 0.3)) {
+                        isOpen.toggle()
+                    }
+                }
+            ZStack {
+                // ここに設定を追加する。
+                SwiftUI.List {
+                    Text("Settings")
+                    Text("Settings")
+                    Text("Settings")
+                }
+                // 設定のフッター
+                VStack {
+                    Spacer()
+                    Text("Hello, World!")
+                }
+                .foregroundStyle(.secondary)
+                .padding()
+            }
+            .padding(.leading, maxWidth / 4)
+            .offset(x: isOpen ? 0 : maxWidth)
+            .animation(.easeInOut(duration: 0.3), value: isOpen)
+        }
+    }
+}
--- TodoDaily/Shared/Layout/BaseScreenContainer.swift
+++ TodoDaily/Shared/Layout/BaseScreenContainer.swift
@@ -6,16 +6,16 @@ struct BaseScreenContainer: View {
     @State
     private var selectedTab: TabType = .home
     
-    // 設定Drawer表示/非表示を保持します。
+    // 設定表示/非表示を保持します。
     @State
-    private var isSettingsPresented: Bool = false
+    private var isSettingsOpen: Bool = false
     
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
                 AppHeaderView(
                     onSettingsTapped: {
-                        isSettingsPresented = true
+                        isSettingsOpen = true
                     }
                 )
                 ZStack {
@@ -25,25 +25,7 @@ struct BaseScreenContainer: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 AppFooterView(selectedTab: $selectedTab)
             }
-            
-            // 設定Drawerを表示
-            if isSettingsPresented {
-                Color.black.opacity(0.3)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        withAnimation {
-                            isSettingsPresented = false
-                        }
-                    }
-                HStack {
-                    Spacer()
-                    SettingsDrawerView(isOpen: $isSettingsPresented)
-                        .frame(width: 300)
-                        .transition(.move(edge: .trailing))
-                        .zIndex(1)
-                }
-                .animation(.easeInOut, value: isSettingsPresented)
-            }
+            SettingsView(isOpen: $isSettingsOpen)
         }
     }
 }
